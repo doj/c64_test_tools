9ff827cd76b5b6551324f859ef204934e27dd3ba
Dirk Jagdmann <doj@cubic.org>
2020-06-29 10:19:10 -0700

    add 4164 pinout to wiring diagram. add 4 LEDs on port A.

--- c64_dram_test/c64_dram_test.ino
+++ c64_dram_test/c64_dram_test.ino
@@ -1,38 +1,37 @@
+// -*- c-basic-offset: 2; tab-width: 2; indent-tabs-mode: nil; mode: c++ -*-
 /*
   Wiring diagram:
-                                               +-----+
-                  +----[PWR]-------------------| USB |--+
-                  |                            +-----+  |
-                  |         GND/RST2  [ ][ ]            |
-                  |       MOSI2/SCK2  [ ][ ]  A5/SCL[ ] |   C5 
-                  |          5V/MISO2 [ ][ ]  A4/SDA[ ] |   C4 
-                  |                             AREF[ ] |
-                  |                              GND[ ] |
-                  | [ ]N/C                    SCK/13[ ] |   B5
-                  | [ ]IOREF                 MISO/12[ ] |   B4 --- DRAM DIN
-                  | [ ]RST                   MOSI/11[ ]~|   B3 --- DRAM /WE
-                  | [ ]3V3    +---+               10[ ]~|   B2 --- DRAM /RAS
-                  | [ ]5v    -| A |-               9[ ]~|   B1 --- DRAM /CAS
-                  | [ ]GND   -| R |-               8[ ] |   B0 --- RAM DOUT
-                  | [ ]GND   -| D |-                    |
-                  | [ ]Vin   -| U |-               7[ ] |   D7 --- RAM A7
-                  |          -| I |-               6[ ]~|   .
-              C0  | [ ]A0    -| N |-               5[ ]~|   .
-              C1  | [ ]A1    -| O |-               4[ ] |   .
-              C2  | [ ]A2     +---+           INT1/3[ ]~|   .
-              C3  | [ ]A3                     INT0/2[ ] |   .
-              C4  | [ ]A4/SDA  RST SCK MISO     TX>1[ ] |   .
-              C5  | [ ]A5/SCL  [ ] [ ] [ ]      RX<0[ ] |   D0 --- RAM A0
-                  |            [ ] [ ] [ ]              |
-                  |  UNO_R3    GND MOSI 5V  ____________/
-                  \_______________________/
-      
-                  http://busyducks.com/ascii-art-arduinos
+                                           +-----+
+            +----[PWR]---------------------| USB |----+
+            |                              +-----+    |
+            |           GND/RST2  [ ][ ]              |
+            |         MOSI2/SCK2  [ ][ ]    A5/SCL[ ] |
+            |            5V/MISO2 [ ][ ]    A4/SDA[ ] |
+            |                                 AREF[ ] |
+            |                                  GND[ ] |    --- RAM GND, pin 16
+            | [ ]N/C        4116/4864       SCK/13[ ] | B5 (LED)
+            | [ ]IOREF      +---U---+      MISO/12[ ] | B4 --- RAM DIN (D), pin 2
+            | [ ]RST     n/c|1    16|GND   MOSI/11[ ]~| B3 --- RAM /WE, pin 3
+            | [ ]3V3       D|2    15|/CAS       10[ ]~| B2 --- RAM /RAS, pin 4
+RAM VCC --- | [ ]5v      /WE|3    14|Q           9[ ]~| B1 --- RAM /CAS, pin 15
+pin 8       | [ ]GND    /RAS|4    13|A6          8[ ] | B0 --- RAM DOUT (Q), pin 14
+            | [ ]GND      A0|5    12|A3               |
+            | [ ]Vin      A2|6    11|A4          7[ ] | D7 --- RAM A7, pin 9
+            |             A1|7    10|A5          6[ ] | D6 --- RAM A6, pin 13
+LED test 0  | [ ]A0   5V,VCC|8     9|A7          5[ ] | D5 --- RAM A5, pin 10
+LED test 1  | [ ]A1         +-------+            4[ ] | D4 --- RAM A4, pin 11
+LED test CB | [ ]A2                         INT1/3[ ] | D3 --- RAM A3, pin 12
+LED test RCB| [ ]A3                         INT0/2[ ] | D2 --- RAM A2, pin 6
+            | [ ]A4/SDA     RST SCK MISO      TX>1[ ] | D1 --- RAM A1, pin 7
+            | [ ]A5/SCL     [ ] [ ] [ ]       RX<0[ ] | D0 --- RAM A0, pin 5
+            |               [ ] [ ] [ ]               |
+            |  UNO_R3       GND MOSI 5V _____________/
+            \__________________________/
 
-  Remaining DRAM pins.
+http://busyducks.com/ascii-art-arduinos
 
-  - pin 8  (Vcc) -> +5v
-  - pin 16 (Vss) -> GND
+The 4 LEDs on Port A need to be connected to GND with a resistor. For
+regular 5V LEDs a resistor of 220 Ohm or 470 Ohm should be used.
 
   This is for Hitachi HM4864 chips. See C64 service manual for compatible chips.
 
@@ -47,6 +46,9 @@
 
   2. Run this test:
 
+  On startup the program will light up the 5 LEDs in sequence, to verify that the LEDs are working.
+  Then they are turned off and testing starts.
+
   It will write 1s, then 0s, then two checkerboard patterns in a never-ending loop.
 
   RAM is automatically refreshed on row selection. As long as every row is selected once
@@ -57,6 +59,10 @@
   - LED (pin 13) off -> TEST PASS
   - LED (pin 13) on  -> TEST FAIL (will turn back off at the end of the test cycle, so PAY ATTENTION)
 
+  - LED port A0 -> write 0 failed
+  - LED port A1 -> write 1 failed
+  - LED port A2 -> checkerboard failed
+  - LED port A3 -> inverse checkerboard failed
 */
 
 #define DOUT 0
@@ -64,7 +70,13 @@
 #define nRAS 2
 #define nWE  3
 #define DIN  4
-#define LED  5
+#define LED_13  5
+
+// LEDs on port A
+#define LED_TEST0 0
+#define LED_TEST1 1
+#define LED_TESTCB 2
+#define LED_TESTRCB 3
 
 #define SETB(port, b)  port |= _BV(b)
 #define CLRB(port, b)  port &= ~_BV(b)
@@ -75,29 +87,58 @@
 #define TEST_CHECKERBOARD
 #define TEST_REVERSE_CHECKERBOARD
 
-void setup() {
-  DDRD    = 0b11111111; // 1 = out, 0 = in
-  // PD7  (7) 1-------; A7
-  // PD6  (6) -1------; A6
-  // PD5  (5) --1-----; A5
-  // PD4  (4) ---1----; A4
-  // PD3  (3) ----1---; A3
-  // PD2  (2) -----1--; A2
-  // PD1  (1) ------1-; A1
-  // PD0  (0) -------1; A0
-  
+void setup()
+{
+  // light up each LED for 200ms
+  pinMode(A0, OUTPUT);
+  pinMode(A1, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(A3, OUTPUT);
+  pinMode(13, OUTPUT);
+
+  digitalWrite(A0, HIGH);
+  delay(200);
+  digitalWrite(A1, HIGH);
+  delay(200);
+  digitalWrite(A2, HIGH);
+  delay(200);
+  digitalWrite(A3, HIGH);
+  delay(200);
+  digitalWrite(13, HIGH);
+  delay(200);
+
+  // turn off LEDs
+  digitalWrite(A0, LOW);
+  digitalWrite(A1, LOW);
+  digitalWrite(A2, LOW);
+  digitalWrite(A3, LOW);
+
+  // configure RAM chip
+  // bit for the data direction config:
+  // 1 = out, 0 = in
+
+  DDRD    = 0b11111111;
+  // PD7  (7) 1------- A7
+  // PD6  (6) -1------ A6
+  // PD5  (5) --1----- A5
+  // PD4  (4) ---1---- A4
+  // PD3  (3) ----1--- A3
+  // PD2  (2) -----1-- A2
+  // PD1  (1) ------1- A1
+  // PD0  (0) -------1 A0
+
+  PORTD   = 0;
+
   DDRB    = 0b11111110;
-  // PB5 (13) --1----- LED
+  // PB5 (13) --1----- LED_13
   // PB4 (12) ---1---- DIN
   // PB3 (11) ----1--- nWE
   // PB2 (10) -----1-- nRAS
   // PB1 ( 9) ------1- nCAS
   // PB0 ( 8) -------0 DOUT
 
-  PORTD   = 0;
-
   PORTB   = 0b11011110;
-  // PB4 (13) --0----- LED = 0
+  // PB4 (13) --0----- LED_13 = 0
   // PB4 (12) ---1---- DIN = 1
   // PB3 (11) ----1--- nWE = read
   // PB2 (10) -----1-- nRAS = 1
@@ -106,7 +147,7 @@ void setup() {
 }
 
 inline void
-w(uint8_t row, uint8_t col, uint8_t v) 
+w(const uint8_t row, const uint8_t col, const uint8_t v)
 {
     if (v) {
       SETB(PORTB, DIN);
@@ -125,7 +166,7 @@ w(uint8_t row, uint8_t col, uint8_t v)
 }
 
 inline uint8_t
-r(uint8_t row, uint8_t col)
+r(const uint8_t row, const uint8_t col)
 {
     // read
     PORTD = row;
@@ -135,8 +176,8 @@ r(uint8_t row, uint8_t col)
 
     asm("nop"); // > 100n
     asm("nop");
-    
-    uint8_t v = ISSET(PINB, DOUT);
+
+    const uint8_t v = ISSET(PINB, DOUT);
 
     SETB(PORTB, nRAS);
     SETB(PORTB, nCAS);
@@ -144,11 +185,11 @@ r(uint8_t row, uint8_t col)
     return v;
 }
 
-void loop() {
+void loop()
+{
   uint8_t col = 0, row = 0, v = 0;
-
-  for (;;) {
-
+  while(1)
+    {
 #ifdef TEST_1
     // WRITE 1s
     do {
@@ -162,7 +203,11 @@ void loop() {
     // VERIFY
     do {
       do {
-        if (! r(row, col)) SETB(PORTB, LED);
+        if (! r(row, col))
+          {
+            SETB(PORTB, LED_13);
+            digitalWrite(A1, HIGH);
+          }
         row++;
       } while (row);
       col++;
@@ -182,7 +227,11 @@ void loop() {
     // VERIFY
     do {
       do {
-        if (r(row, col)) SETB(PORTB, LED);
+        if (r(row, col))
+          {
+            SETB(PORTB, LED_13);
+            digitalWrite(A0, HIGH);
+          }
         row++;
       } while (row);
       col++;
@@ -206,7 +255,11 @@ void loop() {
     v = 0;
     do {
       do {
-        if (r(row, col) != v) SETB(PORTB, LED);
+        if (r(row, col) != v)
+          {
+            SETB(PORTB, LED_13);
+            digitalWrite(A2, HIGH);
+          }
         row++;
         v = !v;
       } while (row);
@@ -232,7 +285,11 @@ void loop() {
     v = 1;
     do {
       do {
-        if (r(row, col) != v) SETB(PORTB, LED);
+        if (r(row, col) != v)
+          {
+            SETB(PORTB, LED_13);
+            digitalWrite(A3, HIGH);
+          }
         row++;
         v = !v;
       } while (row);
@@ -242,6 +299,6 @@ void loop() {
 #endif
 
     delay(100);
-    CLRB(PORTB, LED);
-  }
+    CLRB(PORTB, LED_13);
+    }
 }
